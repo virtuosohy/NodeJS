652d8e390a9f9564915bac5976cc082dc4a73fe1
virtuosohy <virtuosohy@outlook.com>
2024-11-11 15:39:55 +0800

    11.11

--- 创建脚手架/lib/core/action.js
+++ 创建脚手架/lib/core/action.js
@@ -1,6 +1,23 @@
-const myAction = (project,args) =>{
+const inquirer = require('inquirer')
+const config = require('../../config')
+const download = require('download-git-repo')  
+const myAction = async (project,args) =>{
 // console.log('创建项目',project)
 // console.log('其他参数',args)
-
+  const answer =  await inquirer.prompt([
+    {
+      type:'list',
+      name:'famework',
+      choices:config.framwork,
+      message:'请选择框架'
+    }
+   ])
+  //  console.log(answer);
+   //下载代码
+download('direct:'+config.framworkUrl[answer.framwork],project,{clone:true},(err)=>{
+  console.log(err);
+  
+})
+   
 }
 module.exports = myAction;
